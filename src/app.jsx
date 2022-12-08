8f53da331292ba89828e733045c4deade672e690
MarioWork <mariovieira.work@hotmail.com>
2022-12-07 23:55:45 -0100

    use custom hook and create basic form

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,23 @@
+import { form } from "../data/form.json";
+import styled from "styled-components";
+import useMultiStepForm from "./hooks/use-multi-step-form";
+
 const App = () => {
-  return <></>;
+  const { group, onNext, onPrevious } = useMultiStepForm(form);
+
+  return (
+    <StyledGroup>
+      {group.title}
+      <button onClick={onPrevious}>previous</button>
+      <button onClick={onNext}>next</button>
+    </StyledGroup>
+  );
 };
 
 export default App;
+
+const StyledGroup = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100vw;
+`;
