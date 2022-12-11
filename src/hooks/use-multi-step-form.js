0800bc57ba070405cdeb352a8ac74ed4bc6a84c1
MarioWork <mariovieira.work@hotmail.com>
2022-12-11 22:12:42 -0100

    used the new dir for prop types

--- src/hooks/use-multi-step-form.js
+++ src/hooks/use-multi-step-form.js
@@ -1,5 +1,5 @@
 import { useReducer } from "react";
-import formSchema from "../domain/form-prop-types";
+import formSchema from "../schemas/form-prop-types";
 
 const Actions = {
     NEXT: "next",
