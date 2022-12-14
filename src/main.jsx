ed78d7bd9d212bdf1c1d3ffd7780a8474ed8d351
MarioWork <mariovieira.work@hotmail.com>
2022-12-14 19:34:21 -0100

    add to previous commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app";
-import GlobalStyles from "./global-styles";
+import GlobalStyles from "./styles/global-styles";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
