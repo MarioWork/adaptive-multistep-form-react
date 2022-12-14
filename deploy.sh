6ced69a8d1663a11f2a372c44d0b38a1b1631f85
MarioWork <mariovieira.work@hotmail.com>
2022-12-13 23:34:48 -0100

    fix the deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,9 @@
 # abort on errors
 set -e
 
+# If it is a redeploy uncomment the line bellow
+rm -rf dist
+
 # build
 npm run build
 
@@ -18,7 +21,7 @@ echo > .nojekyll
 git init
 git checkout -B main
 git add -A
-git commit -m 'deploy'
+git commit -m 'deploy2'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
