551a385ffe4742b235a7bd06b8d7be79e6425c63
MarioWork <mariovieira.work@hotmail.com>
2022-12-14 19:14:49 -0100

    create input component

---
+++ src/components/question/common/question-input.jsx
@@ -0,0 +1,24 @@
+import { string } from "prop-types";
+
+const QuestionInput = ({ type }) => {
+  return <StyledInput type={type} />;
+};
+
+QuestionInput.propTypes = {
+  type: string.isRequired,
+};
+
+const StyledInput = styled.input`
+  border-radius: 25px;
+  padding: 0.75em;
+  font-size: 0.9em;
+  border: none;
+  outline: none;
+
+  &:focus {
+    outline: auto;
+    outline-color: #c69749;
+  }
+`;
+
+export default QuestionInput;
