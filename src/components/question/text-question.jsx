ed85652b6bd36bed615fa5a6b374f367e7282787
MarioWork <mariovieira.work@hotmail.com>
2022-12-14 19:39:22 -0100

    first version of question container ui

--- src/components/question/text-question.jsx
+++ src/components/question/text-question.jsx
@@ -1,19 +1,28 @@
-import React from "react";
 import QuestionSchema from "../../schemas/question-prop-types";
 import QuestionTitle from "./common/question-title";
 import QuestionInput from "./common/question-input";
+import { QuestionType } from "../../enum/question-types";
+import styled from "styled-components";
 
 const TextQuestion = ({ question }) => {
   return (
-    <>
+    <StyledQuestionContainer>
       <QuestionTitle title={question.title} />
-      <QuestionInput type={text} />
-    </>
+      <QuestionInput type={QuestionType.TEXT} />
+    </StyledQuestionContainer>
   );
 };
 
 TextQuestion.propTypes = {
   question: QuestionSchema,
 };
+
+const StyledQuestionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  padding: 0.5em;
+  gap: 10px;
+`;
 
 export default TextQuestion;
