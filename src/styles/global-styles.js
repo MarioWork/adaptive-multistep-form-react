12fe27d7a74f7aefe56774a5c19c7831677cf9fc
MarioWork <mariovieira.work@hotmail.com>
2022-12-14 19:33:59 -0100

    add to previous commit

---
+++ src/styles/global-styles.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+        font-family: "Roboto", sans-serif;
+        color: #282a3a;
+
+    }
+
+    body {
+        height: 100vh;
+        background-color: #282A3A;
+        padding-top: 5em;
+    }
+`;
+
+export default GlobalStyles;
