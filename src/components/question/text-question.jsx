ebdb52949d4d8c82787681a6d6acbf721e7f14c5
MarioWork <mariovieira.work@hotmail.com>
2022-12-14 19:15:06 -0100

    use input in text question

--- src/components/question/text-question.jsx
+++ src/components/question/text-question.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import QuestionSchema from "../../schemas/question-prop-types";
 import QuestionTitle from "./common/question-title";
+import QuestionInput from "./common/question-input";
 
 const TextQuestion = ({ question }) => {
   return (
     <>
       <QuestionTitle title={question.title} />
-      <input type="text" />
+      <QuestionInput type={text} />
     </>
   );
 };
