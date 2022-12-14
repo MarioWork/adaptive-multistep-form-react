729b151da31e9e1eb97cef7e3718969181022916
MarioWork <mariovieira.work@hotmail.com>
2022-12-14 19:35:19 -0100

    add rules to title component overflow

--- src/components/question/common/question-title.jsx
+++ src/components/question/common/question-title.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { string } from "prop-types";
+import styled from "styled-components";
 
 const QuestionTitle = ({ title }) => {
-  return <h4>{title}</h4>;
+  return <StyledTitle>{title}</StyledTitle>;
 };
 
 QuestionTitle.propTypes = {
   title: string.isRequired,
 };
+
+const StyledTitle = styled.h4`
+  font-size: 1.1em;
+  max-lines: 2;
+  text-overflow: ellipsis;
+`;
 
 export default QuestionTitle;
