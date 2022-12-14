e94da90873c37fb15b4346f1df6ba2fbfbb9a6f1
MarioWork <mariovieira.work@hotmail.com>
2022-12-13 23:27:38 -0100

    adapt question list to use new question components

--- src/components/question/question-list.jsx
+++ src/components/question/question-list.jsx
@@ -2,16 +2,19 @@ import React from "react";
 import questionSchema from "../../schemas/question-prop-types";
 import { arrayOf } from "prop-types";
 import { QuestionType } from "../../enum/question-types";
+import TextQuestion from "./text-question";
+import NumberQuestion from "./number-question";
+import DateQuestion from "./date-question";
 
 const questionMap = (question) => {
   const { id, type } = question;
   switch (type) {
     case QuestionType.TEXT:
-      return <QuestionComponent key={id} question={question} />;
+      return <TextQuestion key={id} question={question} />;
     case QuestionType.NUMBER:
-      return <QuestionComponent key={id} question={question} />;
+      return <NumberQuestion key={id} question={question} />;
     case QuestionType.DATE:
-      return <QuestionComponent key={id} question={question} />;
+      return <DateQuestion key={id} question={question} />;
   }
 };
 
@@ -22,8 +25,3 @@ const QuestionList = ({ questions }) => {
 QuestionList.propTypes = { questions: arrayOf(questionSchema) };
 
 export default QuestionList;
-
-const QuestionComponent = ({ question }) => {
-  const { title } = question;
-  return <p>{title}</p>;
-};
