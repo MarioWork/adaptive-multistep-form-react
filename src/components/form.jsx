d39ba5b239f0416e0e523bd77bc873afc85acb6b
MarioWork <mariovieira.work@hotmail.com>
2022-12-15 00:44:15 -0100

    fix styling with form using new group component

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -2,28 +2,24 @@ import React from "react";
 import styled from "styled-components";
 import formSchema from "../schemas/form-prop-types";
 import useMultiStepForm from "../hooks/use-multi-step-form";
-import QuestionList from "./question/question-list";
+import Group from "./group";
 
 const Form = ({ form }) => {
   const { group, onNext, onPrevious } = useMultiStepForm(form);
 
   return (
     <StyledForm>
-      <h1>{form.title}</h1>
-      {form?.description && <h5>{form.description}</h5>}
-      <StyledGroup>
-        <header>
-          {group?.title && <h3>{group.title}</h3>}
-          {group?.description && <h5>{group.description}</h5>}
-        </header>
-        <main>
-          <QuestionList questions={group?.questions} />
-        </main>
-        <footer>
-          <button onClick={onPrevious}>Previous</button>
-          <button onClick={onNext}>Next</button>
-        </footer>
-      </StyledGroup>
+      <header>
+        <h1>{form.title}</h1>
+        {form?.description && <h5>{form.description}</h5>}
+      </header>
+      <main>
+        <Group group={group} />
+      </main>
+      <footer>
+        <button onClick={onPrevious}>Previous</button>
+        <button onClick={onNext}>Next</button>
+      </footer>
     </StyledForm>
   );
 };
@@ -46,37 +42,11 @@ const StyledForm = styled.main`
   box-shadow: rgb(153 153 153 / 25%) 10px 0px 70px;
   background: #bebebe;
 
-  h1,
-  h5 {
-    opacity: 0.7;
-    display: -webkit-box;
-    -webkit-line-clamp: 2;
-    -webkit-box-orient: vertical;
-    overflow: hidden;
-    text-overflow: ellipsis;
-  }
-
-  h1 {
-    opacity: 1;
-  }
-`;
-
-const StyledGroup = styled.article`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 100%;
-  min-height: 90%;
-  max-height: 90%;
-  padding-top: 1em;
-
   header {
     min-height: 10%;
     max-height: 10%;
-    max-width: 100%;
-    text-align: center;
 
-    h3,
+    h1,
     h5 {
       opacity: 0.7;
       display: -webkit-box;
@@ -86,26 +56,21 @@ const StyledGroup = styled.article`
       text-overflow: ellipsis;
     }
 
-    h3 {
+    h1 {
       opacity: 1;
     }
   }
 
   main {
     width: 100%;
-    max-width: 100%;
     min-height: 85%;
     max-height: 85%;
-    display: flex;
-    padding: 0.5em;
-    flex-direction: column;
-    overflow-y: auto;
   }
 
   footer {
     display: flex;
     justify-content: space-evenly;
-    align-items: flex-end;
+    align-items: flex-start;
     width: 100%;
     padding-bottom: 1em;
     min-height: 10%;
