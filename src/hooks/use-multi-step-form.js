9023493bb3de1f65986266e2f71ef5f8c0d33cd2
MarioWork <mariovieira.work@hotmail.com>
2022-12-16 00:26:00 -0100

    prepare hook for answer action

--- src/hooks/use-multi-step-form.js
+++ src/hooks/use-multi-step-form.js
@@ -4,21 +4,22 @@ import formSchema from "../schemas/form-prop-types";
 const Actions = {
     NEXT: "next",
     PREVIOUS: "previous",
+    ANSWER: "Answer",
 }
 
 const useMultiStepForm = ({ groups }) => {
 
-    const initialState = { currentIndex: 0 }
+    const initialState = { currentIndex: 0, answers: [] }
 
-    const canIncrement = (max, currentIndex) => currentIndex + 1 < max;
-    const canDecrement = (min, currentIndex) => currentIndex - 1 >= min;
+    const canNext = (max, currentIndex) => currentIndex + 1 < max;
+    const canPrevious = (min, currentIndex) => currentIndex - 1 >= min;
 
-    const reducer = (state, action) => {
+    const reducer = (state, { action, payload }) => {
         switch (action) {
             case Actions.NEXT:
-                return canIncrement(groups.length, state.currentIndex) ? { ...state, currentIndex: state.currentIndex + 1 } : state;
+                return canNext(groups.length, state.currentIndex) ? { ...state, currentIndex: state.currentIndex + 1 } : state;
             case Actions.PREVIOUS:
-                return canDecrement(0, state.currentIndex) ? { ...state, currentIndex: state.currentIndex - 1 } : state;
+                return canPrevious(0, state.currentIndex) ? { ...state, currentIndex: state.currentIndex - 1 } : state;
         }
     }
 
@@ -26,8 +27,9 @@ const useMultiStepForm = ({ groups }) => {
 
     return {
         group: groups[state.currentIndex],
-        onNext: () => dispatch(Actions.NEXT),
-        onPrevious: () => dispatch(Actions.PREVIOUS),
+        onNext: () => dispatch({ action: Actions.NEXT }),
+        onAnswer: (questionId, answer) => dispatch({ action: Actions.ANSWER, payload: { questionId, answer } }),
+        onPrevious: () => dispatch({ action: Actions.PREVIOUS }),
     };
 };
 
