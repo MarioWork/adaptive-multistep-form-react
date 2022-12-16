e22869e9ce5baa9d3e0631bf37bae552cf0b6ae6
MarioWork <mariovieira.work@hotmail.com>
2022-12-16 00:10:46 -0100

    add question container to date and number question

--- src/components/question/type/number-question.jsx
+++ src/components/question/type/number-question.jsx
@@ -1,8 +1,13 @@
 import QuestionSchema from "../../../schemas/question-prop-types";
 import QuestionTitle from "../common/question-title";
+import QuestionContainer from "../common/question-container";
 
 const NumberQuestion = ({ question }) => {
-  return <QuestionTitle title={question.title} />;
+  return (
+    <QuestionContainer>
+      <QuestionTitle title={question.title} />
+    </QuestionContainer>
+  );
 };
 
 NumberQuestion.propTypes = {
