4a1a95044b1d2753688571bddb3a3cbbded0d1f3
MarioWork <mariovieira.work@hotmail.com>
2022-12-18 22:22:46 -0100

    add input type number to question number

--- src/components/question/type/number-question.jsx
+++ src/components/question/type/number-question.jsx
@@ -1,11 +1,14 @@
 import QuestionSchema from "../../../schemas/question-prop-types";
 import QuestionTitle from "../common/question-title";
+import QuestionInput from "../common/question-input";
 import QuestionContainer from "../common/question-container";
+import { QuestionType } from "../../../enum/question-types";
 
 const NumberQuestion = ({ question }) => {
   return (
     <QuestionContainer>
       <QuestionTitle title={question.title} />
+      <QuestionInput type={QuestionType.NUMBER} questionId={question.id} />
     </QuestionContainer>
   );
 };
