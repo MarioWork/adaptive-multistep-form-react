87bf30855585b5bf4dfebf0ea1d7059b89df288e
MarioWork <mariovieira.work@hotmail.com>
2022-12-18 22:23:02 -0100

    centralize the string of input type

--- src/components/question/type/date-question.jsx
+++ src/components/question/type/date-question.jsx
@@ -2,12 +2,13 @@ import QuestionSchema from "../../../schemas/question-prop-types";
 import QuestionTitle from "../common/question-title";
 import QuestionInput from "../common/question-input";
 import QuestionContainer from "../common/question-container";
+import { QuestionType } from "../../../enum/question-types";
 
 const DateQuestion = ({ question }) => {
   return (
     <QuestionContainer>
       <QuestionTitle title={question.title} />
-      <QuestionInput type="date" questionId={question.id} />
+      <QuestionInput type={QuestionType.DATE} questionId={question.id} />
     </QuestionContainer>
   );
 };
