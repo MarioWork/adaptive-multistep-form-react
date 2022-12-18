ed9a5fae00b3d61601e2eaf7084a809758b9ef1a
MarioWork <mariovieira.work@hotmail.com>
2022-12-18 22:18:32 -0100

    use input date in date question

--- src/components/question/type/date-question.jsx
+++ src/components/question/type/date-question.jsx
@@ -1,11 +1,13 @@
 import QuestionSchema from "../../../schemas/question-prop-types";
 import QuestionTitle from "../common/question-title";
+import QuestionInput from "../common/question-input";
 import QuestionContainer from "../common/question-container";
 
 const DateQuestion = ({ question }) => {
   return (
     <QuestionContainer>
       <QuestionTitle title={question.title} />
+      <QuestionInput type="date" questionId={question.id} />
     </QuestionContainer>
   );
 };
